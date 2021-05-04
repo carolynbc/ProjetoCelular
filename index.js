e1e482d78f49b0818af4a59e10abc14b90a1a10a
Carolyn Bispo <69880804+carolynbc@users.noreply.github.com>
2021-05-04 19:40:21 -0300

    Add files via upload
    versao_2_js

--- index.js
+++ index.js
@@ -1,48 +1,46 @@
-document.addEventListener('deviceready', onDeviceReady, false);
+let meuapp = {
 
-console.log ("Teste para ver console.log no celular");//aqui eu só coloco para ver se aparece
+    contatoSelecionado: null,
 
-function onDeviceReady() {
-   //sempre deletar tudo o que vem aqui dentro para começar do início
-    
-    var btnTirarFoto = document.getElementById("btnTirarFoto"); //aqui estou pegando o botão para usar
-    btnTirarFoto.addEventListener('click', tirarFoto); //adicionei um evento, uma função 
-  
-}
+    inicializar: function () {
+        console.log("inicializar: function(){...");
+        document.addEventListener('deviceready', meuapp.onMyDeviceReady, false);
+        document.addEventListener('resume', meuapp.retornoAoPrimeiroPlano, false);
+    },
 
-function tirarFoto(){
+    onMyDeviceReady: function () {
+        console.log('#####=> Running cordova' + cordova.platformId + '@' + cordova.version);
+        document.getElementById("btnSelecionaContato").addEventListener('click', meuapp.selecionarContato);
+    },
 
-    if(!navigator.camera){
-        alert ("Erro, o plugin do cordova não foi instalado!");
+    retornoAoPrimeiroPlano: function () {
+        console.log("#### => retornoAoPrimeiroPlano:function(){...");
+    },
+
+    selecionarContato: function () {
+        console.log("selecionarContato : function(){...");
+        navigator.contacts.pickContact(function (c) {
+            console.log("####=> Contato Selecionado");
+            console.log(c);
+            meuapp.contatoSelecionado = c;
+            console.log(c.displayName);
+            let spanElement = document.getEelementById("nomeDoContato").innerHTML = c.displayName;
+            console.log(spanElement);
+            console.log(c.photos);
+            let imgElement = document.getElementById("imgDoContato");
+            console.log("Tag Imagem...");
+            console.log(imgElement);
+            console.log("Value do obj Imagem do array photos");
+            console.log(c.photos[0].value);
+            imgElement.src = c.photos[0].value;
+            //imgElement.src = c.photos[0].value;
+        }, function (err) {
+            console.log("####=> Contato nao Selecionado");
+            console.log(err);
+        });
+
     }
 
-    var options = {
-        quality: 50,
-        destinationType: Camera.DestinationType.DATA_URL,
-        encodingType: Camera.EncodingType.JPEG,
-        mediaType: Camera.MediaType.PICTURE,
-        saveToPhotoAlbum: true,
-        sourceType: 1, // 0:Photo Library, 1=Camera, 2=Saved Album (opções de escolha)
-        encodingType: 0 // 0=JPG 1=PNG (opções de escolha)
-    };
-
-    let options2 = {
-        qualidade: 50 ,
-        destinationType: Camera.DestinationType.DATA_URL,
-        allowEdit: true,
-        saveToPhotoAlbum: false,
-        cameraDirection: 1,
-        sourceType: Camera.PictureSourceType.CÂMERA //é a mesma coisa que o numero 1 no options
-    } ;
-    
-    navigator.camera.getPicture(
-        function(imgData){//isso daqui fará efeito la na imagem que aparecerá no html para nós
-            let imgHtmlTag = document.getElementById("imgHtmlTag");
-            imgHtmlTag.src = "data:image/jpeg;base64,"+imgData; //aqui será a imagem que eu irei receber, aqui a imagem aparecerá
-        },
-        function(err){
-            alert(err);
-        },
-        options
-    );
 }
+
+meuapp.inicializar();
